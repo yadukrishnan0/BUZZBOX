6a5bcc615cf3d0207b51d56844d5b6290fecff9f
yadukrishnan <yadhu4507@gmail.com>
2024-04-16 17:32:48 +0530

    login page design

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/User/Login";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
